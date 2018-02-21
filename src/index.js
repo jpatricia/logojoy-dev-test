5bf41fbff54684c0a7a238da13d11875e4404292
jpatricia <janice_patricia@live.com>
2018-02-21 15:23:11 -0500

    added starting code

---
+++ src/index.js
@@ -0,0 +1,7 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import './index.css';
+import App from './App';
+import superagent from 'superagent';
+
+ReactDOM.render(<App />, document.getElementById('root'));
