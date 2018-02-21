4750bdefa2598d3c9b3a26340758ad87ae6a74cd
jpatricia <janice_patricia@live.com>
2018-02-21 17:13:25 -0500

    added logo with hover share btn

--- src/App.js
+++ src/App.js
@@ -1,12 +1,21 @@
 import React, { Component } from 'react';
 import './App.css';
+import logo from './Images/logo-lg.png';
+import shareButton from './Images/button.png';
 
 class App extends Component {
 
   render() {
     return (
       <div className="App">
-        Hello World
+        <div className="container center">
+          <div className="logo">
+            <img src={logo}/>
+          </div>
+          <div className="overlay center">
+            <button className="share-btn center">Share</button>
+          </div>
+        </div>
       </div>
     );
   }
