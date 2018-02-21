b80fcbd3984166969cc154ec6e5de100ca33eb64
jpatricia <janice_patricia@live.com>
2018-02-21 18:12:36 -0500

    added button onclick

--- src/App.js
+++ src/App.js
@@ -4,8 +4,22 @@ import logo from './Images/logo-lg.png';
 import shareButton from './Images/button.png';
 
 class App extends Component {
+  constructor() {
+    super();
+    this.state = {
+      dialogOpen: false
+    }
+    this.shareDialogBox = this.shareDialogBox.bind(this);
+  }
+
+  shareDialogBox() {
+    this.setState ({
+      dialogOpen : !this.state.dialogOpen
+    })
+  }
 
   render() {
+    const open = this.state.dialogOpen;
     return (
       <div className="App">
         <div className="container center">
@@ -13,7 +27,12 @@ class App extends Component {
             <img src={logo}/>
           </div>
           <div className="overlay center">
-            <button className="share-btn center">Share</button>
+            <button onClick={this.shareDialogBox} className="share-btn center">Share</button>
+          </div>
+        </div>
+        <div className={open ? 'dialogBox open' : 'dialogBox'}>
+          <div className="dialogBox-content">
+            some stuff
           </div>
         </div>
       </div>
