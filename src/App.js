fe565552f6a55a31623e52cc008af558dddf761d
jpatricia <janice_patricia@live.com>
2018-02-24 02:59:00 -0500

    added images and state

--- src/App.js
+++ src/App.js
@@ -6,6 +6,13 @@ import twitter from './Images/twitter.png';
 import facebook from './Images/facebook.png';
 import email from './Images/email.png';
 import group from './Images/Group.png';
+import shirt from './Images/t-shirt-lg.png';
+import sm from './Images/sm-lg.png';
+import bc from './Images/bc-lg.png';
+import logoThmb from './Images/logo-thmb.png';
+import shirtThmb from './Images/t-shirt-thmb.png';
+import smThmb from './Images/sm-thmb.png';
+import bcThmb from './Images/bc-thmb.png';
 
 const socialMedia = [
   {name: 'twitter', img: twitter, link: 'https://twitter.com'},
@@ -14,23 +21,46 @@ const socialMedia = [
   {name: 'group', img: group, link: 'https://logojoy.com/'}
 ];
 
+const images = [
+  {name: 'logo', thmb: logoThmb, img: logo},
+  {name: 'shirt', thmb: shirtThmb, img: shirt},
+  {name: 'bc', thmb: bcThmb, img: bc},
+  {name: 'sm', thmb: smThmb, img: sm}
+];
+
 class App extends Component {
   constructor() {
     super();
     this.state = {
-      dialogOpen: false
-    }
+      dialogOpen: false,
+      imageActive: 'logo'
+    };
     this.shareDialogBox = this.shareDialogBox.bind(this);
+    this.onHover = this.onHover.bind(this);
+    this.onMouseOut = this.onMouseOut.bind(this);
   }
 
   shareDialogBox() {
     this.setState ({
       dialogOpen : !this.state.dialogOpen
-    })
+    });
+  }
+
+  onHover(imgName) {
+    this.setState ({
+      imageActive: imgName
+    });
+  }
+
+  onMouseOut() {
+    this.setState ({
+      imageActive: 'logo'
+    });
   }
 
   render() {
     const open = this.state.dialogOpen;
+    const {imageActive} = this.state;
     return (
       <div className="App">
         <div className="container center">
@@ -48,7 +78,7 @@ class App extends Component {
             </button>
             <div className="content-container">
               <div className="social-medias">
-                Share with:
+                <span>Share with:</span>
                 {socialMedia.map(e => {
                   return (
                     <a href={e.link}>
@@ -60,11 +90,21 @@ class App extends Component {
               </div>
               <div className="main-content">
                 <div className="left-col">
-                  stuff
+                  {images.map(i => {
+                    return(
+                      <img src={i.thmb} onMouseOver={() => this.onHover(i.name)} onMouseOut={this.onMouseOut}/>
+                    )
+                    })
+                  }
                 </div>
                 <div className="right-col">
                   <div className="big-img">
-
+                    {images.map(i => {
+                      return(
+                        <img src={i.img} className={imageActive === i.name ? 'active' : 'none' }/>
+                      )
+                      })
+                    }
                   </div>
                   <div className="info">
                     <h2>The #1 Free Logo Maker - Logojoy</h2>
